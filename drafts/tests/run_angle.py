29d8f189623a0c81273ed05497e56588762074ec
me15degrees <me15degrees@gmail.com>
2024-06-28 22:38:40 -0300

    style: changing directory's name

---
+++ drafts/tests/run_angle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+from time import sleep
+
+""" DOCUMENTAÇÃO: Runs the motor at a constant speed by a given angle.
+
+COMENTÁRIOS: gira no ângulo das rodas do motor
+"""
+ev3 = EV3Brick()
+
+class Sumo:
+    def __init__(self, velocidade, wheel_diameter, wheel_distance):
+        self.velocidade = velocidade
+        self.wheel_diameter = wheel_diameter
+        self.wheel_lenght = wheel_diameter * 3.1415
+        self.wheel_distance = wheel_distance
+        self.r_motor = Motor(Port.A) 
+        self.l_motor = Motor(Port.B)
+        self.r1_motor = Motor(Port.D) # Motores que levam sinais de contrário na frente deles
+        self.l1_motor = Motor(Port.C) # Motores que levam sinais de contrário na frente deles
+        self.ultra_sens1 = UltrasonicSensor(Port.S1) # Sensor ultrassônico frontal direito
+        self.ultra_sens2 = UltrasonicSensor(Port.S2) # Sensor ultrassônico frontal esquerdo
+     
+    def walk(self, speed=300): # Andar para frente, vale ressaltar que os motores de cada lado estão espelhados
+        self.r_motor.run(speed)
+        self.l_motor.run(speed)
+        self.r1_motor.run(-speed)
+        self.l1_motor.run(-speed)
+    
+    def run_angle(self, speed, rotation_angle):
+        self.r_motor.run_angle(speed, rotation_angle, then=Stop.HOLD, wait=True)
+        self.l_motor.run_angle(speed, rotation_angle, then=Stop.HOLD, wait=True)
+        self.r1_motor.run_angle(-speed, rotation_angle, then=Stop.HOLD, wait=True)
+        self.l1_motor.run_angle(-speed, rotation_angle, then=Stop.HOLD, wait=True)
+
+robo_sumo = Sumo(100,4.5,11.8)
+
+def main():
+    flag = 0
+    while not flag:  # Espera pelo botão central.
+        for button in ev3.buttons.pressed():
+            print(button)
+            if button == Button.CENTER:
+                flag = 1
+    while True:
+        robo_sumo.run_angle(100,40)
+main()
